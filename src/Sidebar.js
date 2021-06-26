bebac450e17f7230719d7d1e4ce051756c2d4ede
Dheemanth Narasimha <dheemanth.simha01@gmail.com>
2021-06-26 11:14:56 +0530

    Remove snooze button

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './Sidebar.css';
-import {Button, IconButton} from "@material-ui/core";
+import { Button, IconButton } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import SidebarOption from './SidebarOption';
 import InboxIcon from "@material-ui/icons/Inbox";
 import StarIcon from "@material-ui/icons/Star";
-import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import LabelImportantIcon from "@material-ui/icons/LabelImportant";
 import NearMeIcon from "@material-ui/icons/NearMe";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
@@ -14,28 +13,27 @@ import DuoIcon from "@material-ui/icons/Duo";
 import PhoneIcon from "@material-ui/icons/Phone";
 // import NoteIcon from "@material-ui/icons/Note";
 
-function Sidebar ()  {
+function Sidebar() {
     return (
         <div className="sidebar">
-            <Button startIcon={<AddIcon fontSize="large" />}className="sidebar-compose">Compose</Button>
-            <SidebarOption Icon ={InboxIcon} Title="Inbox" number={54} selected="true"/>
-            <SidebarOption Icon ={StarIcon} Title="Starred" number={54}/>
-            <SidebarOption Icon ={AccessTimeIcon} Title="Snoozed" number={54}/>
-            <SidebarOption Icon ={LabelImportantIcon} Title="Important" number={54}/>
-            <SidebarOption Icon ={NearMeIcon} Title="Sent" number={54}/>
+            <Button startIcon={<AddIcon fontSize="large" />} className="sidebar-compose">Compose</Button>
+            <SidebarOption Icon={InboxIcon} Title="Inbox" number={54} selected="true" />
+            <SidebarOption Icon={StarIcon} Title="Starred" number={54} />
+            <SidebarOption Icon={LabelImportantIcon} Title="Important" number={54} />
+            <SidebarOption Icon={NearMeIcon} Title="Sent" number={54} />
             {/* <SidebarOption Icon ={NoteIcon} Title="Drafts" number={54}/> */}
-            <SidebarOption Icon ={ExpandMoreIcon} Title="More" number={54}/>
+            <SidebarOption Icon={ExpandMoreIcon} Title="More" number={54} />
 
             <div className="sidebar-footer">
                 <div className="sidebar-footerIcons">
                     <IconButton>
-                        <PersonIcon/>
+                        <PersonIcon />
                     </IconButton>
                     <IconButton>
-                        <DuoIcon/>
+                        <DuoIcon />
                     </IconButton>
                     <IconButton>
-                        <PhoneIcon/>
+                        <PhoneIcon />
                     </IconButton>
                 </div>
             </div>
