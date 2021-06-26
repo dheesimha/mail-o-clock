0854bde6cb749fec7f5b39d23e8374545f8efc53
Sahas R <sahasr369@gmail.com>
2021-06-26 23:47:13 +0530

    Add send mail feature

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ import {
   Switch,
   Route,
   Link
-} from "react-router-dom";
+ } from "react-router-dom";
 import Mail from './Mail';
 import EmailList from './EmailList';
+import SendMail from "./sendMail";
+import { useSelector } from "react-redux"
+import { selectSendMessageIsOpen} from "./features/mailSlice";
 
 function App() {
-  return (
+  const sendMessageIsOpen =  useSelector(selectSendMessageIsOpen);
+
+  return(
     <Router>
     <div className="App">
       <Header />
@@ -27,6 +32,8 @@ function App() {
         </Route>
       </Switch>
     </div>
+
+    {sendMessageIsOpen && <SendMail/>}
     </div>
     </Router>
   );
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import './Sidebar.css';
+import React from "react";
+import "./Sidebar.css";
 import { Button, IconButton } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
-import SidebarOption from './SidebarOption';
+import SidebarOption from "./SidebarOption";
 import InboxIcon from "@material-ui/icons/Inbox";
 import StarIcon from "@material-ui/icons/Star";
 import LabelImportantIcon from "@material-ui/icons/LabelImportant";
@@ -11,34 +11,49 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import PersonIcon from "@material-ui/icons/Person";
 import DuoIcon from "@material-ui/icons/Duo";
 import PhoneIcon from "@material-ui/icons/Phone";
+import { useDispatch } from "react-redux";
+import { openSendMessage } from "./features/mailSlice";
 // import NoteIcon from "@material-ui/icons/Note";
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            <Button startIcon={<AddIcon fontSize="large" />} className="sidebar-compose">Compose</Button>
-            <SidebarOption Icon={InboxIcon} Title="Inbox" number={54} selected="true" />
-            <SidebarOption Icon={StarIcon} Title="Starred" number={54} />
-            <SidebarOption Icon={LabelImportantIcon} Title="Important" number={54} />
-            <SidebarOption Icon={NearMeIcon} Title="Sent" number={54} />
-            {/* <SidebarOption Icon ={NoteIcon} Title="Drafts" number={54}/> */}
-            <SidebarOption Icon={ExpandMoreIcon} Title="More" number={54} />
+  const dispatch = useDispatch();
 
-            <div className="sidebar-footer">
-                <div className="sidebar-footerIcons">
-                    <IconButton>
-                        <PersonIcon />
-                    </IconButton>
-                    <IconButton>
-                        <DuoIcon />
-                    </IconButton>
-                    <IconButton>
-                        <PhoneIcon />
-                    </IconButton>
-                </div>
-            </div>
+  return (
+    <div className="sidebar">
+      <Button
+        startIcon={<AddIcon fontSize="large" />}
+        className="sidebar-compose"
+        onClick={() => dispatch(openSendMessage())}
+      >
+        Compose
+      </Button>
+      <SidebarOption
+        Icon={InboxIcon}
+        Title="Inbox"
+        number={54}
+        selected="true"
+      />
+      <SidebarOption Icon={StarIcon} Title="Starred" number={54} />
+      <SidebarOption Icon={LabelImportantIcon} Title="Important" number={54} />
+      <SidebarOption Icon={NearMeIcon} Title="Sent" number={54} />
+      {/* <SidebarOption Icon ={NoteIcon} Title="Drafts" number={54}/> */}
+      <SidebarOption Icon={ExpandMoreIcon} Title="More" number={54} />
+
+      <div className="sidebar-footer">
+        <div className="sidebar-footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
